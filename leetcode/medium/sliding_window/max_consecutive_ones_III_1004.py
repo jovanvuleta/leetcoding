0c2107432f2e3427a248056864f9b290f3696287
jovanvuleta <jocavuleta@gmail.com>
2023-06-06 10:40:17 +0200

    add: 1004. Max Consecutive Ones III

--- leetcode/medium/sliding_window/max_consecutive_ones_III_1004.py
+++ leetcode/medium/sliding_window/max_consecutive_ones_III_1004.py
@@ -8,9 +8,36 @@ class Solution:
             Time complexity: O(N)
             Space complexity: O(1)
         """
-        pass
+        l, r = 0, 0
+        count = 0
+        zero_count = 0
+
+        for r in range(len(nums)):
+            if nums[r] == 0:
+                zero_count += 1
+
+            while zero_count > k:
+                if nums[l] == 0:
+                    zero_count -= 1
+                l += 1
+
+            count = max(count, r - l + 1)
+
+        return count
+
+    @staticmethod
+    def longestOnesAnotherSolution(self, nums: List[int], k: int) -> int:
+        l = 0
+
+        for r, n in enumerate(nums):
+            k -= (1 - n)
+            if k < 0:
+                k += (1 - nums[l])
+                l += 1
+
+        return r - l + 1
 
 
 if __name__ == "__main__":
-    result = Solution.longestOnes(self=Solution, nums=[1], k=1)
+    result = Solution.longestOnesAnotherSolution(self=Solution, nums=[0, 0, 1, 1, 0, 0, 1, 1, 1, 0, 1, 1, 0, 0, 0, 1, 1, 1, 1], k=2)
     print(result)
