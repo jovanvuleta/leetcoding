5d883fc524990cb59c23ba1905a1db97b1debeaa
jovanvuleta <jocavuleta@gmail.com>
2023-06-11 12:02:07 +0200

    add: 1802. Maximum Value at a Given Index in a Bounded Array

--- leetcode/medium/binary_search/maximum_value_at_a_given_index_in_a_bounded_array_1802.py
+++ leetcode/medium/binary_search/maximum_value_at_a_given_index_in_a_bounded_array_1802.py
@@ -1,6 +1,11 @@
 class Solution:
     @staticmethod
     def maxValue(self, n: int, index: int, maxSum: int) -> int:
+        """
+            Time complexity: O(log(maxSum) * log(n))
+            Space complexity: O(1)
+        """
+
         def check(a):
             left_offset = max(a - index, 0)
             result = (a + left_offset) * (a - left_offset + 1) // 2
@@ -17,6 +22,49 @@ class Solution:
             else:
                 right = mid - 1
         return left + 1
+
+    @staticmethod
+    def maxValueSecondSolution(self, n: int, index: int, maxSum: int) -> int:
+        res = 1
+        left, right = 0, maxSum
+
+        def sequence(attempt):
+            total = 0
+            # can fit sequence on left side
+            if index + 1 >= attempt:
+                total += attempt * (attempt + 1) // 2
+                left_of_sequence = index - attempt
+                total += left_of_sequence + 1
+            # can't fit sequence on left side
+            else:
+                total += attempt * (attempt + 1) // 2
+                left_of_sequence = attempt - (index + 1)
+                total -= left_of_sequence * (left_of_sequence + 1) // 2
+
+            end = n - 1
+            # can fit sequence on right side
+            if(end - index + 1) >= attempt:
+                total += attempt * (attempt + 1) // 2
+                right_of_sequence = index + attempt
+                total += end - right_of_sequence + 1
+            # can't fit sequence on right side
+            else:
+                total += attempt * (attempt + 1) // 2
+                right_of_sequence = attempt - (end + 1 - index)
+                total -= right_of_sequence * (right_of_sequence + 1) // 2
+
+            total -= attempt
+            return total
+
+        while left <= right:
+            mid = (left + right) // 2
+            if sequence(mid) <= maxSum:
+                res = max(res, mid)
+                left = mid + 1
+            else:
+                right = mid - 1
+
+        return res
 
 
 if __name__ == "__main__":
