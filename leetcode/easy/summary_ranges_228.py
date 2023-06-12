a9786ff594112ac674a4237fd015a6fb814c168e
jovanvuleta <jocavuleta@gmail.com>
2023-06-12 07:49:40 +0200

    add: 228. Summary Ranges

---
+++ leetcode/easy/summary_ranges_228.py
@@ -0,0 +1,34 @@
+from typing import List
+
+
+class Solution:
+    @staticmethod
+    def summaryRanges(self, nums: List[int]) -> List[str]:
+        if not nums:
+            return []
+
+        res = []
+        start = nums[0]
+        end = nums[0]
+
+        for i in range(1, len(nums)):
+            if nums[i] == end + 1:
+                end = nums[i]
+            else:
+                if start != end:
+                    res.append(f"{start}->{end}")
+                else:
+                    res.append(str(start))
+                start = nums[i]
+                end = nums[i]
+
+        if start != end:
+            res.append(f"{start}->{end}")
+        else:
+            res.append(str(start))
+
+        return res
+
+
+if __name__ == "__main__":
+    print(Solution.summaryRanges(self=Solution, nums=[0, 2, 3, 4, 6, 8, 9]))
