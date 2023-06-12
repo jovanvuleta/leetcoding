826dadf1bab489956c6e54aff3d4f7fe2b06e55c
jovanvuleta <jocavuleta@gmail.com>
2023-06-12 08:17:03 +0200

    add: 1207. Unique Number of Occurrences

---
+++ leetcode/easy/hash_set/unique_number_of_occurences_1207.py
@@ -0,0 +1,24 @@
+from collections import Counter
+from typing import List
+
+
+class Solution:
+    @staticmethod
+    def uniqueOccurrences(self, arr: List[int]) -> bool:
+        count = {}
+
+        for num in arr:
+            count[num] = count.get(num, 0) + 1
+
+        set_size = set(count.values())
+
+        return len(count) == len(set_size)
+
+    @staticmethod
+    def uniqueOccurrencesSecond(self, arr: List[int]) -> bool:
+        dec = Counter(arr)
+        return len(dec) == len(set(dec.values()))
+
+
+if __name__ == "__main__":
+    print(Solution.uniqueOccurrences(self=Solution, arr=[1, 2, 2, 1, 1, 3]))
