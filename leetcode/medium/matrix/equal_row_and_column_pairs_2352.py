c879db4fe084b13ef628a3f4ed014e4c00413494
jovanvuleta <jocavuleta@gmail.com>
2023-06-13 07:58:56 +0200

    add: 2352. Equal Row and Column Pairs

--- leetcode/medium/matrix/equal_row_and_column_pairs_2352.py
+++ leetcode/medium/matrix/equal_row_and_column_pairs_2352.py
@@ -23,6 +23,34 @@ class Solution:
             total += counter[tuple(now)]
         return total
 
+    @staticmethod
+    def equalPairsSecond(self, grid: List[List[int]]) -> int:
+        rows = grid
+        columns = list(zip(*grid))
+        counter = 0
+        for i in rows:
+            for k in columns:
+                if i == list(k):
+                    counter += 1
+        return counter
+
+    @staticmethod
+    def equalPairsThird(self, grid: List[List[int]]) -> int:
+        n = len(grid)
+        ans = 0
+
+        for i in range(n):
+            for j in range(n):
+                k = 0
+                while k < n:
+                    if grid[i][k] != grid[k][j]:
+                        break
+                    k += 1
+                if k == n:  # R[i] == C[j]
+                    ans += 1
+
+        return ans
+
 
 if __name__ == "__main__":
     print(Solution.equalPairs(self=Solution, grid=[
