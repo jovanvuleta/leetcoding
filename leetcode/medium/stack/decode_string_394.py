c68e9feb1ed178720996fdda93064a7d0026b734
jovanvuleta <jocavuleta@gmail.com>
2023-06-15 09:01:19 +0200

    add: 394. Decode String

---
+++ leetcode/medium/stack/decode_string_394.py
@@ -0,0 +1,29 @@
+class Solution:
+    @staticmethod
+    def decodeString(self, s: str) -> str:
+        """
+            Time complexity: O(N)
+            Space complexity: O(N)
+        """
+        stack = []
+
+        for i in range(len(s)):
+            if s[i] != "]":
+                stack.append(s[i])
+            else:
+                substr = ""
+                while stack[-1] != "[":
+                    substr = stack.pop() + substr
+                stack.pop()
+
+                k = ""
+                while stack and stack[-1].isdigit():
+                    k = stack.pop() + k
+
+                stack.append(int(k) * substr)
+
+        return "".join(stack)
+
+
+if __name__ == "__main__":
+    print(Solution.decodeString(self=Solution, s="3[a]2[bc]"))
