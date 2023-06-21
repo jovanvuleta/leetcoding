987abb95182e74a3ea8e568b36ccf177dccba412
jovanvuleta <jocavuleta@gmail.com>
2023-06-21 09:35:41 +0200

    add: 1448. Count Good Nodes in Binary Tree

--- leetcode/medium/tree/count_good_nodes_in_binary_tree_1448.py
+++ leetcode/medium/tree/count_good_nodes_in_binary_tree_1448.py
@@ -1,4 +1,7 @@
 # Definition for a binary tree node.
+from collections import deque
+
+
 class TreeNode:
     def __init__(self, val=0, left=None, right=None):
         self.val = val
@@ -13,6 +16,7 @@ class Solution:
             Time complexity: O(N)
             Space complexity: O(H) - height of the tree
         """
+
         def dfs(node, maxVal):
             if not node:
                 return 0
@@ -24,6 +28,26 @@ class Solution:
             return res
 
         return dfs(root, root.val)
+
+    @staticmethod
+    def goodNodesSecond(self, root: TreeNode) -> int:
+        ans = 0
+        q = deque()
+
+        q.append((root, root.val))
+
+        while q:
+            node, maxval = q.popleft()
+            if node.val >= maxval:
+                ans += 1
+
+            if node.left:
+                q.append((node.left, max(maxval, node.val)))
+
+            if node.right:
+                q.append((node.right, max(maxval, node.val)))
+
+        return ans
 
 
 if __name__ == "__main__":
