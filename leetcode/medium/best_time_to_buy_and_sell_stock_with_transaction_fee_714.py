40475a059346e7f4ee701ca11564a31478faaefa
jovanvuleta <jocavuleta@gmail.com>
2023-06-22 07:57:41 +0200

    add: 714. Best Time to Buy and Sell Stock with Transaction Fee

---
+++ leetcode/medium/best_time_to_buy_and_sell_stock_with_transaction_fee_714.py
@@ -0,0 +1,39 @@
+from typing import List
+
+
+class Solution:
+    @staticmethod
+    def maxProfit(self, prices: List[int], fee: int) -> int:
+        """
+            Time complexity: O(N)
+            Space complexity: O(1)
+        """
+        buy = float('-inf')
+        sell = 0
+
+        for price in prices:
+            buy = max(buy, sell - price)
+            sell = max(sell, buy + price - fee)
+
+        return sell
+
+    @staticmethod
+    def maxProfitDpSolution(self, prices: List[int], fee: int) -> int:
+        """
+            Time complexity: O(N)
+            Space complexity: O(N)
+        """
+        n = len(prices)
+        buy = [0] * n
+        sell = [0] * n
+        buy[0] = -prices[0]
+
+        for i in range(1, n):
+            buy[i] = max(buy[i - 1], sell[i - 1] - prices[i])
+            sell[i] = max(sell[i - 1], buy[i - 1] + prices[i] - fee)
+
+        return sell[-1]
+
+
+if __name__ == "__main__":
+    print(Solution.maxProfit(self=Solution, prices=[1, 3, 2, 8, 4, 9], fee=2))
