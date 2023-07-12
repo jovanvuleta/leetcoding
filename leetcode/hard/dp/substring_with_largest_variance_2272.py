8f91c73bbe91921b68020d042df3f32ed17fea7f
jovanvuleta <jocavuleta@gmail.com>
2023-07-12 09:05:50 +0200

    add: 2272. Substring With Largest Variance

--- leetcode/hard/dp/substring_with_largest_variance_2272.py
+++ leetcode/hard/dp/substring_with_largest_variance_2272.py
@@ -1,13 +1,22 @@
+import collections
+import itertools
+
+
 class Solution:
+
     @staticmethod
     def largestVariance(self, s: str) -> int:
+        """
+            Time complexity: O(N^2)
+            Space complexity: O(N)
+        """
         max_variance = 0
 
         # create distinct list of character pairs
         pairs = [(l1, l2) for l1 in set(s) for l2 in set(s) if l1 != l2]
 
         # run once for original string order, then again for reverse string order
-        for runs in range(2):
+        for _ in range(2):
             for pair in pairs:
                 count1 = count2 = 0
                 for letter in s:
@@ -28,6 +37,52 @@ class Solution:
 
         return max_variance
 
+    @staticmethod
+    def largestVarianceSecond(self, s: str) -> int:
+        """
+            Time complexity: O(N^2)
+            Space complexity: O(N)
+        """
+        counter = collections.Counter(s)
+        res = 0
+
+        for char1, char2 in itertools.permutations(counter, 2):
+            char1_count = counter[char1]
+            char2_count = counter[char2]
+
+            diff = 0
+
+            seen_char1 = seen_char2 = False
+
+            for char in s:
+                if char not in (char1, char2):
+                    continue
+
+                if diff < 0:
+                    if not char1_count:
+                        break
+
+                    if not char2_count:
+                        res = max(res, diff + char1_count)
+                        break
+                    seen_char1 = seen_char2 = False
+                    diff = 0
+
+                if char == char1:
+                    seen_char1 = True
+                    char1_count -= 1
+                    diff += 1
+
+                if char == char2:
+                    seen_char2 = True
+                    char2_count -= 1
+                    diff -= 1
+
+                if seen_char1 and seen_char2:
+                    res = max(res, diff)
+
+        return res
+
 
 if __name__ == "__main__":
-    print(Solution.largestVariance(self=Solution, s="aababbb"))
+    print(Solution.largestVarianceSecond(self=Solution, s="aababbb"))
