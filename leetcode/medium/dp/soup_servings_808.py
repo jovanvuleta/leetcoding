fc2b557d7a6880bbb29f5203794a375a8bec3faf
jovanvuleta <jocavuleta@gmail.com>
2023-07-29 08:40:28 +0200

    add: 808. Soup Servings

---
+++ leetcode/medium/dp/soup_servings_808.py
@@ -0,0 +1,25 @@
+class Solution:
+    @staticmethod
+    def soupServings(n: int) -> float:
+        if n > 4800:
+            return 1.0
+        n = (n + 24) // 25
+        memo = dict()
+
+        def dp(i, j):
+            if (i, j) in memo:
+                return memo[(i, j)]
+            if i <= 0 and j <= 0:
+                return 0.5
+            if i <= 0:
+                return 1.0
+            if j <= 0:
+                return 0.0
+            memo[(i, j)] = 0.25 * (dp(i - 4, j) + dp(i - 3, j - 1) + dp(i - 2, j - 2) + dp(i - 1, j - 3))
+            return memo[(i, j)]
+
+        return dp(n, n)
+
+
+if __name__ == "__main__":
+    print(Solution.soupServings(n=50))
