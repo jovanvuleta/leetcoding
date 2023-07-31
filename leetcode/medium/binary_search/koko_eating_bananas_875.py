2c671536a2d120076c93edc9b5acda6cc979279e
jovanvuleta <jocavuleta@gmail.com>
2023-07-31 09:23:05 +0200

    add: 875. Koko Eating Bananas

--- leetcode/medium/binary_search/koko_eating_bananas_875.py
+++ leetcode/medium/binary_search/koko_eating_bananas_875.py
@@ -1,3 +1,4 @@
+import math
 from typing import List
 
 
@@ -8,8 +9,25 @@ class Solution:
             Time complexity: O(N * log(U))
             Space complexity: O(1)
         """
-        pass
+
+        def helper(k):
+            hours = 0
+            for pile in piles:
+                hours += math.ceil(pile / k)
+            return hours <= h
+
+        l, r = 1, max(piles)
+        min_speed = -1
+
+        while l <= r:
+            m = (l + r) // 2
+            if helper(m):
+                min_speed = m
+                r = m - 1
+            else:
+                l = m + 1
+        return min_speed
 
 
 if __name__ == "__main__":
-    print(Solution.minEatingSpeed(self=Solution, piles=[1, 3, 2], h=2))
+    print(Solution.minEatingSpeed(self=Solution, piles=[3, 6, 7, 11], h=8))
