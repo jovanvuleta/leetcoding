1c60309fea00746713e670d0bbe6c40a8241f21c
jovanvuleta <jocavuleta@gmail.com>
2023-08-09 08:49:55 +0200

    add: 2616. Minimize the Maximum Difference of Pairs

---
+++ leetcode/medium/minimize_the_maximum_difference_of_pairs_2616.py
@@ -0,0 +1,42 @@
+from typing import List
+
+
+class Solution:
+    @staticmethod
+    def minimizeMax(nums: List[int], p: int) -> int:
+        """
+            Time complexity: O(N * log(M))
+            Space complexity: O(1)
+        """
+        def is_valid(threshold):
+            # Greedy
+            i, cnt = 0, 0
+            while i < len(nums) - 1:
+                if abs(nums[i] - nums[i + 1]) <= threshold:
+                    cnt += 1
+                    i += 2
+                else:
+                    i += 1
+                if cnt == p:
+                    return True
+            return False
+
+        if p == 0:
+            return 0
+
+        nums.sort()
+        l, r = 0, 10**9
+        res = 10**9
+
+        while l <= r:
+            m = l + (r - l) // 2
+            if is_valid(m):
+                res = m
+                r = m - 1
+            else:
+                l = m + 1
+        return res
+
+
+if __name__ == "__main__":
+    print(Solution.minimizeMax(nums=[], p=2))
