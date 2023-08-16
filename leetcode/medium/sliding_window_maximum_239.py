90a61722f76e564d3bc75828414f5bceb582c7e4
jovanvuleta <jocavuleta@gmail.com>
2023-08-16 09:28:59 +0200

    add: 239. Sliding Window Maximum

---
+++ leetcode/medium/sliding_window_maximum_239.py
@@ -0,0 +1,33 @@
+from collections import deque
+from typing import List
+
+
+class Solution:
+    def maxSlidingWindow(nums: List[int], k: int) -> List[int]:
+        """
+            Time complexity: O(N)
+            Space complexity: O(N)
+        """
+        output = []
+        q = deque()
+        l = r = 0
+
+        while r < len(nums):
+            while q and nums[q[-1]] < nums[r]:
+                q.pop()
+            q.append(r)
+
+            if l > q[0]:
+                q.popleft()
+
+            if (r + 1) >= k:
+                output.append(nums[q[0]])
+                l += 1
+
+            r += 1
+
+        return output
+
+
+if __name__ == "__main__":
+    print(Solution.maxSlidingWindow(nums=[1, 3, -1, -3, 5, 3, 6, 7], k=3))
