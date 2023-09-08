92dc5a227388ab7c41f029fed404233b2ae14d9b
jovanvuleta <jocavuleta@gmail.com>
2023-09-08 11:57:47 +0100

    add: 118. Pascal's Triangle

--- leetcode/easy/dp/pascal_triangle_118.py
+++ leetcode/easy/dp/pascal_triangle_118.py
@@ -18,6 +18,19 @@ class Solution:
             res.append(row)
         return res
 
+    @staticmethod
+    def generateSecondSolution(self, numRows: int) -> List[List[int]]:
+        output = [[1]]
+
+        for r in range(2, numRows + 1):
+            tmp = [1]
+            for c in range(1, r - 1):
+                tmp.append(output[-1][c] + output[-1][c - 1])
+            tmp.append(1)
+            output.append(tmp)
+
+        return output
+
 
 if __name__ == "__main__":
-    print(Solution.generate(self=Solution, numRows=1))
+    print(Solution.generate(self=Solution, numRows=5))
