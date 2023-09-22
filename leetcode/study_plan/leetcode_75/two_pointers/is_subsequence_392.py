b6ed336475e4d9a757bdca16d5969e5ca7090ae1
jovanvuleta <jocavuleta@gmail.com>
2023-09-22 14:01:33 +0100

    add: 392. Is Subsequence

--- leetcode/study_plan/leetcode_75/two_pointers/is_subsequence_392.py
+++ leetcode/study_plan/leetcode_75/two_pointers/is_subsequence_392.py
@@ -18,6 +18,19 @@ class Solution:
 
         return j == len(s)
 
+    def isSubsequenceSecond(self, s: str, t: str) -> bool:
+        if len(s) == 0:
+            return True
+
+        i, j = 0, 0
+
+        while i < len(s) and j < len(t):
+            if s[i] == t[j]:
+                i += 1
+            j += 1
+
+        return i == len(s)
+
 
 if __name__ == "__main__":
     print(Solution.isSubsequence(self=Solution, s="abc", t="ahbgdc"))
