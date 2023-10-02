c68e10ef895e2fe3ca8d99617422ab4251387bd4
jovanvuleta <jocavuleta@gmail.com>
2023-10-02 21:08:07 +0200

    add: 2038. Remove Colored Pieces if Both Neighbors are the Same Color

---
+++ leetcode/easy/string/remove_colored_pieces_if_both_neighbours_are_the_same_color_2038.py
@@ -0,0 +1,13 @@
+import collections
+from itertools import groupby
+
+
+class Solution:
+    def winnerOfGame(self, colors: str) -> bool:
+        c = collections.Counter()
+        for x, t in groupby(colors):
+            c[x] += max(len(list(t)) - 2, 0)
+
+        if c['A'] > c['B']:
+            return True
+        return False
