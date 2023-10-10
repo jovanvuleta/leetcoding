45eca19d900d4ed7e13de99b385e066436c5c8f1
jovanvuleta <jocavuleta@gmail.com>
2023-10-10 09:51:57 +0200

    add: 2009. Minimum Number of Operations to Make Array Continuous

---
+++ leetcode/hard/array/minimum_number_of_operations_to_make_array_continuous_2009.py
@@ -0,0 +1,21 @@
+import sys
+from bisect import bisect_right
+from typing import List
+
+
+class Solution:
+    def minOperations(self, nums: List[int]) -> int:
+        """
+            Time Complexity: O(n ∗ log(n))
+            Space Complexity: O(n)
+        """
+        n = len(nums)
+        nums = sorted(set(nums))
+        ans = sys.maxsize
+
+        for i, s in enumerate(nums):
+            e = s + n - 1
+            idx = bisect_right(nums, e)
+
+            ans = min(ans, n - (idx - i))
+        return ans
