e94a17a389e1f048b4ff613109f5892c113f875b
jovanvuleta <jocavuleta@gmail.com>
2023-11-03 14:41:58 +0000

    add: 1441. Build an Array With Stack Operations

---
+++ leetcode/medium/array/build_an_array_with_stack_operations_1441.py
@@ -0,0 +1,11 @@
+from typing import List
+
+
+class Solution:
+    def buildArray(self, target: List[int], n: int) -> List[str]:
+        initialValue = 1
+        output = []
+        for number in target:
+            output.extend(["Push","Pop"]*(number - initialValue) + ["Push"])
+            initialValue = number + 1
+        return output
